70b06dee599f7a6516269a0dfe29163bc0e7e86b
junyi012 <joey000122@gmail.com>
2023-07-11 17:57:36 -0400

    modified:   noneML_baseline.py 	new file:   test2.png 	new file:   test3.png

--- noneML_baseline.py
+++ noneML_baseline.py
@@ -1,75 +1,105 @@
-import argparse
-import math
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.utils.checkpoint as checkpoint
-from timm.models.layers import DropPath, to_2tuple, trunc_normal_
-import numpy as np
-import matplotlib.pyplot as plt
-import logging
-import torch.nn.init as init
-from math import log10
-import torch.optim as optim
-from torch.optim.lr_scheduler import LambdaLR, StepLR, MultiStepLR, ExponentialLR, ReduceLROnPlateau 
-from torch.utils.data import DataLoader
-from torch.utils.data import Dataset
-from torch.utils.data import random_split
-import torchvision.transforms as transforms
-from PIL import Image, ImageFilter
-from os import listdir
-from os.path import join
-from scipy.io import loadmat
-from tqdm import tqdm
 import h5py
-from src.models import *
-from src.utli import *
-from src.data_loader import getData
+import numpy as np
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
 from scipy.interpolate import CubicSpline
-# trainloader,_,_,_,_ = getData(upscale_factor = 4, 
-#                                 timescale_factor= 4,
-#                                 batch_size = 1000, 
-#                                 crop_size = 256,
-#                                 data_path = "../dataset/nskt16000_1024",
-#                                 num_snapshots = 4,
-#                                 noise_ratio = 0)
-# _,val1,_,_,_ = getData(upscale_factor = 4, 
-#                                 timescale_factor= 4,
-#                                 batch_size = 200, 
-#                                 crop_size = 256,
-#                                 data_path = "../dataset/nskt16000_1024",
-#                                 num_snapshots = 4,
-#                                 noise_ratio = 0)
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
 
-# for i,(data,target) in enumerate(trainloader):
-#     print(data.shape)
-#     print(target.shape)
+def first_t_then_x(file_path, crop, space_scale, time_scale):
+    # Window calculation
+    window = (1024 - crop) // 2
+    window_end = window + crop
+
+    # Loading file and processing data
+    with h5py.File(file_path, 'r') as file:
+        train_hr = file['fields'][()][:, -1, window:window_end, window:window_end]
+
+    x_lr_t_lr = train_hr[::time_scale, ::space_scale, ::space_scale]
+    x_lr_t_hr = train_hr[:, ::space_scale, ::space_scale]
+
+    # Preparing interpolation
+    t_hr = np.linspace(0, 1, train_hr.shape[0])
+    t_lr = np.linspace(0, 1, x_lr_t_lr.shape[0])
+    cs = CubicSpline(t_lr, x_lr_t_lr)
+
+    # Interpolation and reshaping
+    train_poly_pred = torch.tensor(cs(t_hr)).unsqueeze(1)
+    train_poly_truth = torch.tensor(train_hr).unsqueeze(1)
+    pred = F.interpolate(train_poly_pred, size=(crop, crop), mode='bicubic', align_corners=False)
+
+    # Error calculation
+    RFNE = torch.stack([
+        torch.norm(pred[i] - train_poly_truth[i]) / torch.norm(train_poly_truth[i])
+        for i in range(len(pred))
+        if i % time_scale != 0
+    ])
+
+    pred = pred[200:280, 0, 100, 100].numpy()
+    train_poly_truth = train_poly_truth[200:280, 0, 100, 100].numpy()
+
+    plt.figure()
+    plt.plot(pred, marker="o", label="pred")
+    plt.plot(train_poly_truth, marker="o", label="truth")
+    plt.legend()
+    plt.savefig("test2.png")
+
+    plt.figure()
+    plt.plot(
+        train_poly_pred[200:280, 0, 25, 25].squeeze().numpy(),
+        marker="o",
+        label="pred"
+    )
+    plt.plot(x_lr_t_hr[200:280, 25, 25], marker="o", label="truth")
+    plt.legend()
+    plt.savefig("test3.png")
+
+    RFNE2 = torch.stack([
+        torch.norm(
+            torch.tensor(train_poly_pred[i].squeeze()) - torch.tensor(x_lr_t_hr[i])
+        ) / torch.norm(torch.tensor(x_lr_t_hr[i].squeeze()))
+        for i in range(x_lr_t_hr.shape[0])
+        if i % time_scale != 0
+    ])
+
+    return torch.mean(RFNE), torch.mean(RFNE2)
 
 
-# for i,(data,target) in enumerate(val1):
-#     print(data.shape)
-#     print(target.shape)
+def first_x_then_t(file_path, crop, space_scale, time_scale):
+    # Window calculation
+    window = (1024 - crop) // 2
+    window_end = window + crop
 
-file = h5py.File('../dataset/nskt16000_1024/train/nskt_train.h5', 'r')
-crop = 256 
-space_scale = 4 
+    # Loading file and processing data
+    with h5py.File(file_path, 'r') as file:
+        train_hr = file['fields'][()][:, -1, window:window_end, window:window_end]
+
+    x_lr_t_lr = train_hr[::time_scale, ::space_scale, ::space_scale]
+
+    # Preparing interpolation
+    x_lr_t_lr = torch.tensor(x_lr_t_lr).unsqueeze(1)
+    pred_x = F.interpolate(x_lr_t_lr, size=(crop, crop), mode='bicubic', align_corners=False).squeeze()
+    t_hr = np.linspace(0, 1, train_hr.shape[0])
+    t_lr = np.linspace(0, 1, x_lr_t_lr.shape[0])
+    cs = CubicSpline(t_lr, pred_x.numpy())
+    pred = torch.tensor(cs(t_hr)).unsqueeze(1)
+    train_poly_truth = torch.tensor(train_hr).unsqueeze(1)
+
+    # Error calculation
+    RFNE = torch.stack([
+        torch.norm(pred[i] - train_poly_truth[i]) / torch.norm(train_poly_truth[i])
+        for i in range(len(pred))
+        if i % time_scale != 0
+    ])
+
+    return torch.mean(RFNE)
+
+
+# Use the function
+file_path = '../superbench/datasets/nskt16000_1024/train/nskt_train.h5'
+crop = 256
+space_scale = 4
 time_scale = 4
-window_x = (1024-crop)//2
-window_x_end = window_x + crop
-window_y = (1024-crop)//2
-window_y_end = window_y + crop
-train_poly_hr = file['fields'][()][:,-1,window_x:window_x_end,window_y:window_y_end]
-train_poly_lr = train_poly_hr[::4,::space_scale,::space_scale]
-file.close()
-print(train_poly_hr.shape)
-
-t_hr = np.linspace(0,1,train_poly_hr.shape[0])
-t_lr = np.linspace(0,1,train_poly_lr.shape[0])
-# up in time
-cs = CubicSpline(t_lr, train_poly_lr)
-train_poly_pred = torch.tensor(cs(t_hr)).unsqueeze(1)
-pred = F.interpolate(train_poly_pred , size = (256,256), mode = 'bicubic', align_corners=False)
-print(pred.shape)
+mean_RFNE, mean_RFNE2 = first_t_then_x(file_path, crop, space_scale, time_scale)
+# mean_RFNE2 = first_x_then_t(file_path, crop, space_scale, time_scale)
+print(mean_RFNE)
+print(mean_RFNE2)
