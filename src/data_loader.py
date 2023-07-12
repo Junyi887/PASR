b0524dbdf29cded5f9be866d34e25c6edb91a0bc
junyi4090 <joey000122@gmail.com>
2023-07-11 23:49:59 -0700

    modified:   src/data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -8,10 +8,15 @@ import torchvision.transforms as transforms
 from PIL import Image, ImageFilter
 import torchvision.transforms.functional as F
 from torch.utils.data import Dataset, DataLoader, TensorDataset
+import math
+from torch import default_generator, randperm
+from torch._utils import _accumulate
+from torch.utils.data.dataset import Subset
+#"../superbench/datasets/nskt16000_1024"
 
-def getData(data_name = "nskt_16k", data_path = "../superbench/datasets/nskt16000_1024" ,
+def getData(data_name = "nskt_16k", data_path =  "../dedalus/rbc_44/",
              upscale_factor = 4,timescale_factor = 4, num_snapshots = 20,
-             noise_ratio = 0.0, crop_size = 1024, method = "bicubic", 
+             noise_ratio = 0.0, crop_size = 512, method = "bicubic", 
              batch_size = 1, std = [0.6703, 0.6344, 8.3615]):  
     '''
     Loading data from four dataset folders: (a) nskt_16k; (b) nskt_32k; (c) cosmo; (d) era5.
@@ -23,13 +28,19 @@ def getData(data_name = "nskt_16k", data_path = "../superbench/datasets/nskt1600
     ===
     std: the channel-wise standard deviation of each dataset, list: [#channels]
     '''
-
-    train_loader = get_data_loader(data_name, data_path, '/train', "train", upscale_factor, timescale_factor,num_snapshots,noise_ratio, crop_size, method, batch_size, std)
-    val1_loader = get_data_loader(data_name, data_path, '/valid_1', "val", upscale_factor, timescale_factor//2,num_snapshots*2,noise_ratio, crop_size, method, batch_size, std)
-    val2_loader = get_data_loader(data_name, data_path, '/valid_2', "val", upscale_factor,timescale_factor//2,num_snapshots*2,noise_ratio, crop_size, method, batch_size, std) 
-    test1_loader = get_data_loader(data_name, data_path, '/test_1', "test", upscale_factor,timescale_factor//4, num_snapshots*4,noise_ratio, crop_size, method, batch_size, std)
-    test2_loader = get_data_loader(data_name, data_path, '/test_2', "test", upscale_factor,timescale_factor//4, num_snapshots*4, noise_ratio, crop_size, method, batch_size, std)
-    
+    if data_name == "RBC":
+        train_loader = get_data_loader(data_name, data_path, 'train/', "train", upscale_factor, timescale_factor,num_snapshots,noise_ratio, crop_size, method, batch_size, std)
+        val1_loader = get_data_loader(data_name, data_path, '/valid_1', "val", upscale_factor, timescale_factor//2,num_snapshots*2,noise_ratio, crop_size, method, batch_size, std)
+        # val2_loader = get_data_loader(data_name, data_path, '/valid_2', "val", upscale_factor,timescale_factor//2,num_snapshots*2,noise_ratio, crop_size, method, batch_size, std) 
+        # test1_loader = get_data_loader(data_name, data_path, '/test_1', "test", upscale_factor,timescale_factor//4, num_snapshots*4,noise_ratio, crop_size, method, batch_size, std)
+        # test2_loader = get_data_loader(data_name, data_path, '/test_2', "test", upscale_factor,timescale_factor//4, num_snapshots*4, noise_ratio, crop_size, method, batch_size, std)
+    elif data_name == "nskt_16k":
+        train_loader = get_data_loader(data_name, data_path, '/train', "train", upscale_factor, timescale_factor,num_snapshots,noise_ratio, crop_size, method, batch_size, std)
+        val1_loader = get_data_loader(data_name, data_path, '/valid_1', "val", upscale_factor, timescale_factor//2,num_snapshots*2,noise_ratio, crop_size, method, batch_size, std)
+        val2_loader = get_data_loader(data_name, data_path, '/valid_2', "val", upscale_factor,timescale_factor//2,num_snapshots*2,noise_ratio, crop_size, method, batch_size, std) 
+        test1_loader = get_data_loader(data_name, data_path, '/test_1', "test", upscale_factor,timescale_factor//4, num_snapshots*4,noise_ratio, crop_size, method, batch_size, std)
+        test2_loader = get_data_loader(data_name, data_path, '/test_2', "test", upscale_factor,timescale_factor//4, num_snapshots*4, noise_ratio, crop_size, method, batch_size, std)
+    # val1_loader, val2_loader, test1_loader, test2_loader  = 0,0,0,0
     return train_loader, val1_loader, val2_loader, test1_loader, test2_loader 
 
 def get_data_loader(data_name, data_path, data_tag, state, upscale_factor, timescale_factor, num_snapshots,noise_ratio, crop_size, method, batch_size, std):
@@ -54,7 +65,113 @@ def get_data_loader(data_name, data_path, data_tag, state, upscale_factor, times
 
     return dataloader
 
+    if data_name in ['nskt_16k']:
+        dataset = GetFluidDataset(data_path+data_tag, state, transform, upscale_factor,timescale_factor, num_snapshots,noise_ratio, std, crop_size, method) 
+    elif data_name in ['RBC']:
+        dataset = GetRBCDataset(data_path+data_tag, state, transform, upscale_factor,timescale_factor, num_snapshots,noise_ratio, std, crop_size, method) 
+        trianset,val1set = random_split(dataset,[0.9,0.1],generator=torch.Generator().manual_seed(args.seed))
+        testset,val2set = random_split(dataset,[0.9,0.1],generator=torch.Generator().manual_seed(args.seed))
+    if state == "train":
+        shuffle = False
+        
+    else:
+        shuffle = False
 
+
+
+
+class GetRBCDataset(Dataset):
+    '''Dataloader class for NSKT and cosmo datasets'''
+    def __init__(self, location, state, transform, upscale_factor,timescale_factor,num_snapshots, noise_ratio, std,crop_size, method):
+        self.location = location
+        self.upscale_factor = upscale_factor
+        self.state = state
+        self.noise_ratio = noise_ratio
+        self.std = torch.Tensor(std).view(len(std),1,1)
+        self.transform = transform
+        self._get_files_stats()
+        self.crop_size = crop_size
+        self.crop_transform = transforms.CenterCrop(crop_size)
+        self.method = method
+        self.num_snapshots = num_snapshots
+        self.timescale_factor = timescale_factor
+        if method == "bicubic":
+            self.input_transform = transforms.Compose([transforms.CenterCrop(crop_size),
+                                                             transforms.Resize((int(self.crop_size/upscale_factor),int(self.crop_size/upscale_factor)),Image.BICUBIC) ]) # subsampling the image (half size)
+        elif method == "gaussian_blur":
+            self.input_transform = transforms.Compose([transforms.CenterCrop(crop_size), # cropping the image
+                                                       transforms.GaussianBlur(kernel_size=(3,3), sigma=(1,1))])
+        elif method == "uniform":
+            self.input_transform = transforms.Compose([transforms.CenterCrop(crop_size), # cropping the image
+                                        ])
+        self.target_transform = transforms.Compose([transforms.CenterCrop(crop_size) # since it's the target, we keep its original quality
+                                        ])
+    def _get_files_stats(self):
+        self.files_paths = glob.glob(self.location + "/*.h5")
+        self.files_paths.sort()
+        self.n_files = len(self.files_paths)
+        print("Found {} files".format(self.n_files))
+        with h5py.File(self.files_paths[0], 'r') as _f:
+            print("Getting file stats from {}".format(self.files_paths[0]))
+            self.n_samples_per_file = _f['tasks']["vorticity"].shape[0]
+            self.n_in_channels = 1
+            self.img_shape_x = _f['tasks']["vorticity"].shape[1]
+            self.img_shape_y = _f['tasks']["vorticity"].shape[2]
+
+        self.n_samples_total = self.n_files * self.n_samples_per_file
+        self.files = [None for _ in range(self.n_files)]
+        print("Number of samples per file: {}".format(self.n_samples_per_file))
+        print("Found data at path {}. Number of examples: {}. Image Shape: {} x {} x {}".format(
+            self.location, self.n_samples_total, self.img_shape_x, self.img_shape_y, self.n_in_channels))
+
+    def _open_file(self, file_idx):
+        _file = h5py.File(self.files_paths[file_idx], 'r')
+        self.files[file_idx] = _file['tasks']['vorticity']
+
+    def __len__(self):
+        return self.n_samples_total-self.timescale_factor*self.num_snapshots-1
+
+    def __getitem__(self, global_idx):
+        # print("batch_idx: {}".format(batch_idx))
+        y_list = []
+        file_idx, local_idx = self.get_indices(global_idx)
+        # print(file_idx)
+        if self.files[file_idx] is None:
+                self._open_file(file_idx)
+        y = self.transform(self.files[file_idx][local_idx]) # from numpy to torch
+        y = self.target_transform(y).unsqueeze(0) # cropping the image
+        X = self.get_X(y) # getting the input
+        # getting the future samples
+        y_list.append(y)
+        for i in range(1, self.num_snapshots+1):
+            file_idx, local_idx_future = self.get_indices(global_idx + i*self.timescale_factor)
+            #open image file for future sample
+            if self.files[file_idx] is None:
+                self._open_file(file_idx)
+            y = self.transform(self.files[file_idx][local_idx_future])
+            y = self.target_transform(y).unsqueeze(0)
+            y_list.append(y)
+        y = torch.stack(y_list,dim = 0) 
+        return X,y
+
+    def get_indices(self, global_idx):
+        file_idx = int(global_idx/self.n_samples_per_file)  # which file we are on
+        local_idx = int(global_idx % self.n_samples_per_file)  # which sample in that file we are on 
+
+        return file_idx, local_idx
+
+    def get_X(self, y):
+        if self.method == "uniform":
+            X = y[:, ::self.upscale_factor, ::self.upscale_factor]
+        elif self.method == "noisy_uniform":
+            X = y[:, ::self.upscale_factor, ::self.upscale_factor]
+            X = X + self.noise_ratio * self.std * torch.randn(X.shape)
+        elif self.method == "bicubic":
+            X = self.input_transform(y)
+        else:
+            raise ValueError(f"Invalid method: {self.method}")
+        #TODO: add gaussian blur
+        return X
 
 class GetFluidDataset(Dataset):
     '''Dataloader class for NSKT and cosmo datasets'''
@@ -163,6 +280,58 @@ class GetFluidDataset(Dataset):
 
 
 
+def random_split(dataset, lengths,
+                 generator=default_generator):
+    r"""
+    Randomly split a dataset into non-overlapping new datasets of given lengths.
+
+    If a list of fractions that sum up to 1 is given,
+    the lengths will be computed automatically as
+    floor(frac * len(dataset)) for each fraction provided.
+
+    After computing the lengths, if there are any remainders, 1 count will be
+    distributed in round-robin fashion to the lengths
+    until there are no remainders left.
+
+    Optionally fix the generator for reproducible results, e.g.:
+
+    >>> random_split(range(10), [3, 7], generator=torch.Generator().manual_seed(42))
+    >>> random_split(range(30), [0.3, 0.3, 0.4], generator=torch.Generator(
+    ...   ).manual_seed(42))
+
+    Args:
+        dataset (Dataset): Dataset to be split
+        lengths (sequence): lengths or fractions of splits to be produced
+        generator (Generator): Generator used for the random permutation.
+    """
+    if math.isclose(sum(lengths), 1) and sum(lengths) <= 1:
+        subset_lengths: List[int] = []
+        for i, frac in enumerate(lengths):
+            if frac < 0 or frac > 1:
+                raise ValueError(f"Fraction at index {i} is not between 0 and 1")
+            n_items_in_split = int(
+                math.floor(len(dataset) * frac)  # type: ignore[arg-type]
+            )
+            subset_lengths.append(n_items_in_split)
+        remainder = len(dataset) - sum(subset_lengths)  # type: ignore[arg-type]
+        # add 1 to all the lengths in round-robin fashion until the remainder is 0
+        for i in range(remainder):
+            idx_to_add_at = i % len(subset_lengths)
+            subset_lengths[idx_to_add_at] += 1
+        lengths = subset_lengths
+        for i, length in enumerate(lengths):
+            if length == 0:
+                warnings.warn(f"Length of split at index {i} is 0. "
+                              f"This might result in an empty dataset.")
+
+    # Cannot verify that dataset is Sized
+    if sum(lengths) != len(dataset):    # type: ignore[arg-type]
+        raise ValueError("Sum of input lengths does not equal the length of the input dataset!")
+
+    indices = randperm(sum(lengths), generator=generator).tolist()  # type: ignore[call-overload]
+    return [Subset(dataset, indices[offset - length : offset]) for offset, length in zip(_accumulate(lengths), lengths)]
+
+
 
 
 
@@ -175,19 +344,19 @@ if __name__ == "__main__":
     print(input.shape)
     print(target.shape)
     print(idx)
-    for idx, (input,target) in enumerate (val1_loader):
-        input = input
-        target = target
-    print(input.shape)
-    print(target.shape)
-    for idx, (input,target) in enumerate (test1_loader):
-        input = input
-        target = target
-    print(input.shape)
-    print(target.shape)
-    list = []
-    for i in range(2):
-        x = torch.rand(1,1,128)
-        list.append(x)
-    x = torch.stack(list,dim = 0)
-    print(x.shape)
+    # for idx, (input,target) in enumerate (val1_loader):
+    #     input = input
+    #     target = target
+    # print(input.shape)
+    # print(target.shape)
+    # for idx, (input,target) in enumerate (test1_loader):
+    #     input = input
+    #     target = target
+    # print(input.shape)
+    # print(target.shape)
+    # list = []
+    # for i in range(2):
+    #     x = torch.rand(1,1,128)
+    #     list.append(x)
+    # x = torch.stack(list,dim = 0)
+    # print(x.shape)
