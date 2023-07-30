correct normalization feature

src/models/PASR_MLP_G.py
@@ -293,7 +293,7 @@ class SwinTransformerBlock(nn.Module):  #STL
         return x
 
     def extra_repr(self) -> str:
-        return f"dim={self.dim}, input_resolution={self.input_resolution}, num_heads={self.num_heads}, " \
+        return f"dim={self.dim}, input_resolution={self.input_resolution}, num_heads={self.num_heads}, "\
                f"window_size={self.window_size}, shift_size={self.shift_size}, mlp_ratio={self.mlp_ratio}"
 
     def flops(self):
@@ -629,27 +629,30 @@ class UpsampleOneStep(nn.Sequential):   #inside HQ Image reconstruction
         return flops
 
 class Gating(nn.Module):
-    def __init__(self,in_dim):
+    def __init__(self, in_dim, num_layers=3):
         super(Gating, self).__init__()
-        self.model = nn.Sequential(
-            nn.Linear(in_dim, in_dim*2),
-            nn.ReLU(),
-            nn.Linear(in_dim*2, in_dim*2),
-            nn.ReLU(),
-            nn.Linear(in_dim*2, in_dim*2),
-            nn.ReLU(),
-            nn.Linear(in_dim*2, in_dim*2),
-            nn.ReLU(),
-            nn.Linear(in_dim*2, in_dim),
-            nn.Sigmoid()
-        ) 
+        
+        layers = []
+        layers.append(nn.Linear(in_dim, in_dim*2))
+        layers.append(nn.ReLU())
+
+        for _ in range(num_layers):
+            layers.append(nn.Linear(in_dim*2, in_dim*2))
+            layers.append(nn.ReLU())
+
+        layers.append(nn.Linear(in_dim*2, in_dim))
+        layers.append(nn.Sigmoid())
+        
+        self.model = nn.Sequential(*layers)
         self.in_dim = in_dim
+
     def forward(self, x):
         x = self.model(x)
-        return x 
+        return x
+
     
 class Gated_NODE(nn.Module):
-    def __init__(self,in_dim,ode_method ="Euler"):
+    def __init__(self,in_dim,ode_method ="Euler",num_layers=3,gating_method ='leaky'):
         super(Gated_NODE, self).__init__()
 
         self.model = nn.Sequential(
@@ -664,7 +667,8 @@ class Gated_NODE(nn.Module):
             nn.Linear(in_dim*2, in_dim),
             nn.Tanh()
         ) 
-        self.gating = Gating(in_dim)
+        self.gating = Gating(in_dim, num_layers=num_layers)
+        self.gating_method = gating_method
         self.in_dim = in_dim
         self.int_method = ode_method
     def forward(self, x,ode_step = 1,task_dt = 1.0):
@@ -676,7 +680,10 @@ class Gated_NODE(nn.Module):
                 xtemp = torch.empty_like(x).copy_(x)
                 xupdate = self.model(xtemp) 
                 g = self.gating(xupdate)
-                x = (1-g) * x +  h*g * xupdate
+                if self.gating_method == 'leaky':
+                    x = (1-g) * x +  h*g * xupdate
+                else:
+                    x = x + h*g* xupdate
         elif self.int_method == "RK4":
             for i in range(ode_step):
                 f1 = self.model(x)
@@ -722,7 +729,7 @@ class PASR_MLP_G(nn.Module):
                  norm_layer=nn.LayerNorm, ape=False, patch_norm=True,
                  use_checkpoint=False, upscale=2, upsampler='', resi_connection='1conv'
                  ,mean = [0],std = [1],  
-                 ode_method = "Euler",
+                 ode_method = "Euler",gating_layers = 3,gating_method='leaky',
                  **kwargs):
         super(PASR_MLP_G, self).__init__()
         
@@ -741,7 +748,7 @@ class PASR_MLP_G(nn.Module):
         self.upscale = upscale
         self.upsampler = upsampler
         self.window_size = window_size
-
+        self.gating_layers = gating_layers
         #####################################################################################################
         ################################### 1, shallow feature extraction ###################################
         self.conv_first = nn.Conv2d(num_in_ch, embed_dim, 3, 1, 1)

train_no_interpolation.py
@@ -63,13 +63,13 @@ def validation(args,model, val1_loader,val2_loader,device):
             model.eval()
             out_x = model(inputs,task_dt = args.task_dt,n_snapshot = 1,ode_step = args.ode_step,time_evol = False) 
             input_loss = criterion_Data(out_x[:,0,...], target[:,0,...]) # Experiment change to criterion 1
-            out_t = model(inputs,task_dt = args.task_dt//2,n_snapshot = 1,ode_step = args.ode_step//2,time_evol = True) 
+            out_t = model(inputs,task_dt = args.task_dt,n_snapshot = args.n_snapshot,ode_step = args.ode_step,time_evol = True) 
             loss_t = criterion_Data(out_t, target[:,1:,...])
-            RFNE_t = torch.norm(out_t-target[:,1:,...],p=2,dim = (1,2)).mean()
+            RFNE_t = torch.norm(out_t-target[:,1:,...],p=2,dim = (1,2))/torch.norm(target[:,1:,...],p=2,dim = (1,2))
             psnr1 = psnr(out_t,target[:,1:,...])
             target_loss1 += loss_t.item() 
             input_loss1 += input_loss.item()
-            RFNE_loss += RFNE_t.item()
+            RFNE_loss += RFNE_t.mean().item()
             psnr1_loss += psnr1.item()
 
     # target_loss2 = 0 
@@ -182,6 +182,9 @@ parser.add_argument('--epochs', type = int, default= 3)
 parser.add_argument('--dtype', type = str, default= "float32")
 parser.add_argument('--seed',type =int, default= 3407)
 
+parser.add_argument('--gating_layers',type =int, default= 3)
+parser.add_argument('--gating_method',type =str, default= 'leaky')
+parser.add_argument('--normalization',type =str, default= 'True')
 
 parser.add_argument('--n_snapshot',type =int, default= 20)
 parser.add_argument('--down_method', type = str, default= "bicubic") # bicubic 
@@ -217,17 +220,20 @@ if __name__ == "__main__":
                                                       num_snapshots = args.n_snapshot,
                                                       noise_ratio = args.noise_ratio,
                                                       data_name = args.data)
-    mean = [0.1429] 
-    std = [8.3615]
+    if args.normalization == "True":
+        mean,std = getNorm(args)
+        mean = [mean]
+        std = [std]
+    else:
+        mean = [0]
+        std = [1]
     model_list = {"PASR": PASR(upscale=args.scale_factor, in_chans=1, img_size=args.crop_size, window_size=8, depths=[6, 6, 6, 6, 6, 6], embed_dim=180, num_heads=[6, 6, 6, 6, 6, 6], mlp_ratio=2, upsampler=args.upsampler, resi_conv='1conv',mean=mean,std=std).to(device,dtype=data_type),
             "PASR_MLP":PASR_MLP(upscale=args.scale_factor, in_chans=1, img_size=args.crop_size, window_size=8, depths=[6, 6, 6, 6, 6, 6], embed_dim=180, num_heads=[6, 6, 6, 6, 6, 6], mlp_ratio=2, upsampler=args.upsampler, resi_conv='1conv',mean=mean,std=std).to(device,dtype=data_type),
-            "PASR_MLP_G":PASR_MLP_G(upscale=args.scale_factor, in_chans=1, img_size=args.crop_size, window_size=8, depths=[6, 6, 6, 6, 6, 6], embed_dim=180, num_heads=[6, 6, 6, 6, 6, 6], mlp_ratio=2, upsampler=args.upsampler, resi_conv='1conv',mean=mean,std=std).to(device,dtype=data_type),
+            "PASR_MLP_G":PASR_MLP_G(upscale=args.scale_factor, in_chans=1, img_size=args.crop_size, window_size=8, depths=[6, 6, 6, 6, 6, 6], embed_dim=180, num_heads=[6, 6, 6, 6, 6, 6], mlp_ratio=2, upsampler=args.upsampler, resi_conv='1conv',mean=mean,std=std,gating_layers=args.gating_layers,gating_method=args.gating_method).to(device,dtype=data_type),
             "PASR_MLP_small":PASR_MLP(upscale=args.scale_factor, in_chans=1, img_size=args.crop_size, window_size=8, depths=[6, 6, 6, 6], embed_dim=60, num_heads=[6, 6, 6, 6], mlp_ratio=2, upsampler=args.upsampler, resi_conv='1conv',mean=mean,std=std).to(device,dtype=data_type),
-            "PASR_MLP_G_small":PASR_MLP_G(upscale=args.scale_factor, in_chans=1, img_size=args.crop_size, window_size=8, depths=[6, 6, 6, 6], embed_dim=60, num_heads=[6, 6, 6, 6], mlp_ratio=2, upsampler=args.upsampler, resi_conv='1conv',mean=mean,std=std).to(device,dtype=data_type),
-            "PASR_MLP_G_aug":PASR_MLP_G_aug(upscale=args.scale_factor, in_chans=1, img_size=args.crop_size, window_size=8, depths=[6, 6, 6, 6, 6, 6], embed_dim=180, num_heads=[6, 6, 6, 6, 6, 6], mlp_ratio=2, upsampler=args.upsampler, resi_conv='1conv',mean=mean,std=std).to(device,dtype=data_type),
-            "PASR_MLP_G_aug_small":PASR_MLP_G_aug(upscale=args.scale_factor, in_chans=1, img_size=args.crop_size, window_size=8, depths=[6, 6, 6, 6], embed_dim=60, num_heads=[6, 6, 6, 6], mlp_ratio=2, upsampler=args.upsampler, resi_conv='1conv',mean=mean,std=std).to(device,dtype=data_type),
-  
-
+            "PASR_MLP_G_small":PASR_MLP_G(upscale=args.scale_factor, in_chans=1, img_size=args.crop_size, window_size=8, depths=[6, 6, 6, 6], embed_dim=60, num_heads=[6, 6, 6, 6], mlp_ratio=2, upsampler=args.upsampler, resi_conv='1conv',mean=mean,std=std,gating_layers=args.gating_layers,gating_method=args.gating_method).to(device,dtype=data_type),
+            # "PASR_MLP_G_aug":PASR_MLP_G_aug(upscale=args.scale_factor, in_chans=1, img_size=args.crop_size, window_size=8, depths=[6, 6, 6, 6, 6, 6], embed_dim=180, num_heads=[6, 6, 6, 6, 6, 6], mlp_ratio=2, upsampler=args.upsampler, resi_conv='1conv',mean=mean,std=std).to(device,dtype=data_type),
+            # "PASR_MLP_G_aug_small":PASR_MLP_G_aug(upscale=args.scale_factor, in_chans=1, img_size=args.crop_size, window_size=8, depths=[6, 6, 6, 6], embed_dim=60, num_heads=[6, 6, 6, 6], mlp_ratio=2, upsampler=args.upsampler, resi_conv='1conv',mean=mean,std=std).to(device,dtype=data_type),
 
     }
     model = torch.nn.DataParallel(model_list[args.model]).to(device)