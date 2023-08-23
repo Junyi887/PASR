8880288bee572249549dd1a06e7b3d79e98042e3
junyi4090 <joey000122@gmail.com>
2023-08-23 14:14:02 -0700

    modified:   src/utli/utli.py 	new file:   train_no_interpolation_nersc.py

--- src/utli/utli.py
+++ src/utli/utli.py
@@ -4,50 +4,57 @@ import glob
 import os
 import argparse
 # data path in raocp 
-DATA_DIR = {"rbc_diff_IC": ["../rbc_diff_IC/rbc_IC1/*.h5","../rbc_diff_IC/rbc_IC2/*.h5"],
-            "nskt_16k": ["../superbench/datasets/nskt16000_1024/train/*.h5", "../superbench/datasets/nskt16000_1024/valid_1/*.h5"],
-            "rbc_diff_10IC": ["../rbc_diff_IC/rbc_10IC/train/rbc_*_256/rbc_*_256_s9.h5","../rbc_diff_IC/rbc_10IC/test/rbc_*_256/rbc_*_256_s9.h5","../rbc_diff_IC/rbc_10IC/val/rbc_*_256/rbc_*_256_s9.h5"],
-            "climate":["../superbench/train/*.h5","../superbench/val/*.h5","../superbench/test/*.h5"],
-            "rbc_25664": ["../rbc_diff_IC/rbc_256_64/train/rbc_*_25664/rbc_*_25664_s2.h5","../rbc_diff_IC/rbc_256_64/val/rbc_*_25664/rbc_*_25664_s2.h5","../rbc_diff_IC/rbc_256_64/test/rbc_*_25664/rbc_*_25664_s2.h5"],
+# DATA_DIR = {"rbc_diff_IC": ["../rbc_diff_IC/rbc_IC1/*.h5","../rbc_diff_IC/rbc_IC2/*.h5"],
+#             "nskt_16k": ["../superbench/datasets/nskt16000_1024/train/*.h5", "../superbench/datasets/nskt16000_1024/valid_1/*.h5"],
+#             "rbc_diff_10IC": ["../rbc_diff_IC/rbc_10IC/train/rbc_*_256/rbc_*_256_s9.h5","../rbc_diff_IC/rbc_10IC/test/rbc_*_256/rbc_*_256_s9.h5","../rbc_diff_IC/rbc_10IC/val/rbc_*_256/rbc_*_256_s9.h5"],
+#             "climate":["../superbench/train/*.h5","../superbench/val/*.h5","../superbench/test/*.h5"],
+#             "rbc_25664": ["../rbc_diff_IC/rbc_256_64/train/rbc_*_25664/rbc_*_25664_s2.h5","../rbc_diff_IC/rbc_256_64/val/rbc_*_25664/rbc_*_25664_s2.h5","../rbc_diff_IC/rbc_256_64/test/rbc_*_25664/rbc_*_25664_s2.h5"],
+#             }
+DATA_DIR = {"Decay_turb": ["../Decay_Turbulence/*/*.h5"],
+            "rbc":["../rbc_10IC/*/*/*.h5"],
+            "Burger2D":["../Burger2D/*/*/*.h5"],
             }
-
 def CenterCrop(data, crop_size):
     h, w = data.shape[-2], data.shape[-1]
     h_crop, w_crop = (h-crop_size) // 2, (w-crop_size) // 2
     data = data[...,h_crop:h_crop+crop_size, w_crop:w_crop+crop_size]
     return data
 
-def getNorm(args):
+def getNorm(args,method ="min-max"):
     # normalization should be consistant with the normalization in the training
     min = []
     max = []
     window_size = args.crop_size
 
+    if method == "min-max":
+        for data_path in DATA_DIR[args.data]:
+            files_paths = glob.glob(data_path)
+            files_paths.sort()
+            data = []
+            for fp in files_paths:
+                with h5py.File(fp,'r') as _f:
+                    if args.data == "nskt_16k":
+                        HR_data = _f['fields'][()][:,2,:,:]
+                        data.append(CenterCrop(HR_data, window_size))
+                    else:
+                        # TODO need to change to more channel, now only support 1 channel
+                        if args.in_channel ==1:
+                            HR_data = _f['tasks']['vorticity'][()]
+                            data.append(CenterCrop(HR_data, window_size)) 
+                        if args.in_channel ==2:
+                            HR_data_u = _f['tasks']['u'][()]
+                            HR_data_v = _f['tasks']['v'][()]
 
-    for data_path in DATA_DIR[args.data]:
-        files_paths = glob.glob(data_path)
-        files_paths.sort()
-        data = []
-        for fp in files_paths:
-            with h5py.File(fp,'r') as _f:
-                if args.data == "nskt_16k":
-                    HR_data = _f['fields'][()][:,2,:,:]
-                    data.append(CenterCrop(HR_data, window_size))
-                else:
-                    # TODO need to change to more channel, now only support 1 channel
-                    HR_data = _f['tasks']['vorticity'][()]
-                    data.append(CenterCrop(HR_data, window_size)) 
-        data =np.concatenate(data, axis=0)
-        data_min = np.min(data)
-        data_max = np.max(data)
-        min.append(data_min)
-        max.append(data_max)
+            data =np.concatenate(data, axis=0)
+            data_min = np.min(data)
+            data_max = np.max(data)
+            min.append(data_min)
+            max.append(data_max)
     
-    data_min = np.min(np.stack(min,axis=0))
-    data_max = np.max(np.stack(max,axis=0))
-    print(f"min: {data_min}, max: {data_max}")
-    return data_min,data_max
-        
+        data_min = np.min(np.stack(min,axis=0))
+        data_max = np.max(np.stack(max,axis=0))
+        print(f"min: {data_min}, max: {data_max}")
+        return data_min,data_max
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--data', type=str, default='nskt_16k', help='data name')
---
+++ train_no_interpolation_nersc.py
@@ -0,0 +1,297 @@
+import argparse
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.checkpoint as checkpoint
+from timm.models.layers import DropPath, to_2tuple, trunc_normal_
+import numpy as np
+import matplotlib.pyplot as plt
+# 0.005
+import logging
+import torch.nn.init as init
+from math import log10
+import torch.optim as optim
+from torch.optim.lr_scheduler import LambdaLR, StepLR, MultiStepLR, ExponentialLR, ReduceLROnPlateau 
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
+from torch.utils.data import random_split
+import torchvision.transforms as transforms
+from PIL import Image, ImageFilter
+from os import listdir
+from os.path import join
+from scipy.io import loadmat
+from tqdm import tqdm
+import h5py
+from src.models import *
+from src.utli import *
+from src.data_loader import getData
+import logging
+import argparse
+import neptune.new as neptune
+# Set up logging configuration
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+ID = torch.randint(1000,(1,1))
+ 
+# Replace the final print statement
+def psnr(true, pred):
+    mse = torch.mean((true - pred) ** 2)
+    if mse == 0:
+        return float(9999)
+    max_value = torch.max(true)
+    psnr = 20 * torch.log10(max_value / torch.sqrt(mse))
+    if psnr.isnan() or psnr.isinf():
+        return float(0)
+    return psnr
+    
+
+
+
+def validation(args,model, val1_loader,val2_loader,device):
+    if args.loss_type =='L1':
+        criterion_Data = nn.L1Loss().to(device)
+    elif args.loss_type =='L2':
+        criterion_Data = nn.MSELoss().to(device)
+    target_loss1 = 0 
+    input_loss1 = 0
+    RFNE1_loss = 0
+    psnr1_loss = 0
+    for batch in val1_loader: # better be the val loader, need to modify datasets, but we are good for now.
+        with torch.no_grad():
+            inputs, target = batch[0].float().to(device), batch[1].float().to(device)
+            model.eval()
+            out_x = model(inputs,task_dt = args.task_dt,n_snapshot = 1,ode_step = args.ode_step,time_evol = False) 
+            input_loss = criterion_Data(out_x[:,0,...], target[:,0,...]) # Experiment change to criterion 1
+            out_t = model(inputs,task_dt = args.task_dt,n_snapshot = args.n_snapshot,ode_step = args.ode_step,time_evol = True) 
+            loss_t = criterion_Data(out_t, target[:,1:,...])
+            RFNE_t = torch.norm(out_t-target[:,1:,...],p=2,dim = (3,4))/torch.norm(target[:,1:,...],p=2,dim = (3,4))
+            target_loss1 += loss_t.item() 
+            input_loss1 += input_loss.item()
+            RFNE1_loss += RFNE_t.mean().item()
+            psnr1_loss += psnr(out_t, target[:,1:,...]).item()
+    result_loader1 = [input_loss1/len(val1_loader), target_loss1/len(val1_loader), RFNE1_loss /len(val1_loader)]
+    target_loss2 = 0 
+    input_loss2 = 0
+    RFNE2_loss = 0
+    psnr2_loss = 0
+    for batch in val2_loader: # better be the val loader, need to modify datasets, but we are good for now.
+        with torch.no_grad():
+            inputs, target = batch[0].float().to(device), batch[1].float().to(device)
+            model.eval()
+            out_x = model(inputs,task_dt = args.task_dt,n_snapshot = 1,ode_step = args.ode_step,time_evol = False) 
+            input_loss = criterion_Data(out_x[:,0,...], target[:,0,...]) # Experiment change to criterion 1
+            out_t = model(inputs,task_dt = args.task_dt,n_snapshot = args.n_snapshot,ode_step = args.ode_step,time_evol = True) 
+            loss_t = criterion_Data(out_t, target[:,1:,...])
+            RFNE_t = torch.norm(out_t-target[:,1:,...],p=2,dim = (3,4))/torch.norm(target[:,1:,...],p=2,dim = (3,4))
+            target_loss2 += loss_t.item() 
+            input_loss2 += input_loss.item()
+            RFNE2_loss += RFNE_t.mean().item()
+            psnr2_loss += psnr(out_t, target[:,1:,...]).item()
+    result_loader2 = [input_loss2/len(val2_loader), target_loss2/len(val2_loader), RFNE2_loss /len(val2_loader)]
+
+    return result_loader1,result_loader2
+
+def train(args,model, trainloader, val1_loader,val2_loader, optimizer,device,savedpath,run):
+    lamb = args.lamb
+    best_epoch = 0
+    val_list = []
+    val_list_x1 = []
+    val_list_t1 = []
+    val_list_x2 = []
+    val_list_t2 = []
+    train_list = []
+    train_list_x = []
+    train_list_t = []
+    if args.scheduler == 'StepLR':
+        scheduler = StepLR(optimizer, args.lr_step, gamma=args.gamma)
+    elif args.scheduler == "Exp":
+        scheduler = ExponentialLR(optimizer, gamma=args.gamma)
+    elif args.scheduler == "plateau":
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,factor=args.gamma, patience=args.patience, min_lr=5e-6,threshold=0.005)
+    best_loss_val = 1e9
+    if args.loss_type =='L1':
+        criterion_Data = nn.L1Loss().to(device)
+    elif args.loss_type =='L2':
+        criterion_Data = nn.MSELoss().to(device)
+
+    for epoch in range(args.epochs):
+        avg_loss = 0
+        avg_val = 0
+        target_loss = 0
+        input_loss = 0
+        if args.scheduler == 'plateau':
+            lr = optimizer.param_groups[0]["lr"]
+            run['train/lr'].log(lr)
+        else:
+            run['train/lr'].log(scheduler.get_lr())
+        for iteration, batch in enumerate(trainloader):
+            inputs, target = batch[0].float().to(device), batch[1].float().to(device)
+            model.train()
+            optimizer.zero_grad()
+            out_x = model(inputs,task_dt = 1,n_snapshot = 1,ode_step = args.ode_step,time_evol = False)
+            loss_x = criterion_Data(out_x[:,0,...], target[:,0,...])
+            out_t = model(inputs,task_dt = args.task_dt,n_snapshot = args.n_snapshot,ode_step = args.ode_step,time_evol = True)
+            loss_t = criterion_Data(out_t,target[:,1:,:,:,:])
+            target_loss += loss_t.item() 
+            input_loss += loss_x.item()
+            loss = loss_t + lamb*loss_x
+            loss.backward()
+            optimizer.step()
+            avg_loss += loss.item()
+        result_val1,result_val2 = validation(args,model, val1_loader,val2_loader,device)
+        if args.scheduler == "plateau":
+            scheduler.step(result_val1[1])
+        else: 
+            scheduler.step()
+
+        avg_val = result_val1[1] + lamb*result_val2[0]
+        # val_list.append(avg_val)
+        # val_list_x1.append(result_val1[0])
+        # val_list_t1.append(result_val1[1])
+        val_list_x2.append(result_val2[2])
+        # train_list.append(avg_loss/len(trainloader))
+        # train_list_x.append(input_loss/len(trainloader))
+        # train_list_t.append(target_loss/len(trainloader))
+        run['train/train_loss'].log(avg_loss / len(trainloader))
+        run['val/val_loss'].log(avg_val)
+        run['train/train_loss_x'].log(input_loss / len(trainloader))
+        run['train/train_loss_t'].log(target_loss / len(trainloader))
+        run['val/val_loss_x1'].log(result_val1[0])
+        run['val/val_loss_t1'].log(result_val1[1])
+        run['test/test_loss_x1'].log(result_val1[0])
+        run['test/test_loss_t1'].log(result_val1[1])
+        run['val/RFNE'].log(result_val1[2])
+        run['val/PSNR'].log(result_val1[3])
+        run['test/RFNE'].log(result_val2[2])
+        run['test/PSNR'].log(result_val2[3])
+        # print(result_val1[3],result_val2[3])
+        logging.info("Epoch: {} | train loss: {} | val loss: {} | val_x1: {} | val_t1: {} | val_x2: {} | val_t2: {}".format(epoch, avg_loss/len(trainloader), avg_val, result_val1[0], result_val1[1], result_val2[0],result_val2[1]))
+        if avg_val < best_loss_val:
+            best_loss_val = avg_val
+            best_model = model
+            best_epoch = epoch
+            torch.save({
+            'epoch': epoch,
+            'model_state_dict': best_model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            "scheduler_state_dict": scheduler.state_dict(),
+            # 'val_sum': np.array(val_list),
+            # 'train_sum': np.array(train_list),
+            # 'val_x1': np.array(val_list_x1),
+            # 'val_t1': np.array(val_list_t1),
+            # 'val_x2': np.array(val_list_x2),
+            # 'val_t2': np.array(val_list_t2),
+            # 'train_x': np.array(train_list_x),
+            # 'train_t': np.array(train_list_t),
+            },"results/"+savedpath + ".pt" ) # remember to change name for each experiment
+        # validate 
+    return min(val_list_x2),best_epoch
+
+
+
+parser = argparse.ArgumentParser(description='training parameters')
+parser.add_argument('--model', type =str ,default= 'PASR')
+parser.add_argument('--data', type =str ,default= 'rbc_diff_10IC')
+parser.add_argument('--loss_type', type =str ,default= 'L1')
+parser.add_argument('--scale_factor', type = int, default= 4)
+parser.add_argument('--timescale_factor', type = int, default= 4)
+parser.add_argument('--task_dt',type =float, default= 4)
+parser.add_argument('--ode_step',type =int, default= 2)
+parser.add_argument('--ode_method',type =str, default= "Euler")
+parser.add_argument('--in_channels',type = int, default= 1)
+parser.add_argument('--batch_size', type = int, default= 8)
+parser.add_argument('--crop_size', type = int, default= 32, help= 'should be same as image dimension')
+parser.add_argument('--epochs', type = int, default= 3)
+parser.add_argument('--dtype', type = str, default= "float32")
+parser.add_argument('--seed',type =int, default= 3407)
+
+parser.add_argument('--gating_layers',type =int, default= 3)
+parser.add_argument('--gating_method',type =str, default= 'leaky')
+
+parser.add_argument('--normalization',type =str, default= 'False')
+
+parser.add_argument('--gamma',type =float, default= 0.95)
+parser.add_argument('--lr_step',type =int, default= 80)
+parser.add_argument('--patience',type =int, default= 15)
+parser.add_argument('--scheduler',type =str, default= 'StepLR')
+parser.add_argument('--n_snapshot',type =int, default= 20)
+parser.add_argument('--down_method', type = str, default= "bicubic") # bicubic 
+parser.add_argument('--upsampler', type = str, default= "pixelshuffle") # nearest+conv
+parser.add_argument('--noise_ratio', type = float, default= 0.0)
+parser.add_argument('--lr', type = float, default= 1e-4)
+parser.add_argument('--lamb', type = float, default= 0.3)
+parser.add_argument('--data_path',type = str,default = "../rbc_diff_IC")
+args = parser.parse_args()
+logging.info(args)
+
+data_dx = 2*np.pi/2048
+########### loaddata ############
+
+
+
+
+if __name__ == "__main__":
+    if args.dtype =="float32":
+        data_type = torch.float32
+    else: 
+        data_type = torch.float64
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed(args.seed)
+    torch.set_default_dtype(data_type)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    logging.info(device)
+    trainloader,val1_loader,val2_loader,_,_ = getData(upscale_factor = args.scale_factor, 
+                                                      timescale_factor= args.timescale_factor,
+                                                      batch_size = args.batch_size, 
+                                                      crop_size = args.crop_size,
+                                                      data_path = args.data_path,
+                                                      num_snapshots = args.n_snapshot,
+                                                      noise_ratio = args.noise_ratio,
+                                                      data_name = args.data,
+                                                      in_channels=args.in_channels,)
+    if args.normalization == "True":
+        mean,std = getNorm(args)
+        mean = [mean]
+        std = [std]
+    else:
+        mean = [0]
+        std = [1]
+    model_list = {"PASR": PASR(upscale=args.scale_factor, in_chans=args.in_channels, img_size=args.crop_size, window_size=8, depths=[6, 6, 6, 6, 6, 6], embed_dim=180, num_heads=[6, 6, 6, 6, 6, 6], mlp_ratio=2, upsampler=args.upsampler, resi_conv='1conv',mean=mean,std=std).to(device,dtype=data_type),
+            "PASR_MLP":PASR_MLP(upscale=args.scale_factor, in_chans=args.in_channels, img_size=args.crop_size, window_size=8, depths=[6, 6, 6, 6, 6, 6], embed_dim=180, num_heads=[6, 6, 6, 6, 6, 6], mlp_ratio=2, upsampler=args.upsampler, resi_conv='1conv',mean=mean,std=std).to(device,dtype=data_type),
+            "PASR_MLP_G":PASR_MLP_G(upscale=args.scale_factor, in_chans=args.in_channels, img_size=args.crop_size, window_size=8, depths=[6, 6, 6, 6, 6, 6], embed_dim=180, num_heads=[6, 6, 6, 6, 6, 6], mlp_ratio=2, upsampler=args.upsampler, resi_conv='1conv',mean=mean,std=std,gating_layers=args.gating_layers,gating_method=args.gating_method).to(device,dtype=data_type),
+            "PASR_MLP_small":PASR_MLP(upscale=args.scale_factor, in_chans=args.in_channels, img_size=args.crop_size, window_size=8, depths=[6, 6, 6, 6], embed_dim=60, num_heads=[6, 6, 6, 6], mlp_ratio=2, upsampler=args.upsampler, resi_conv='1conv',mean=mean,std=std).to(device,dtype=data_type),
+            "PASR_MLP_G_small":PASR_MLP_G(upscale=args.scale_factor, in_chans=args.in_channels, img_size=args.crop_size, window_size=8, depths=[6, 6, 6, 6], embed_dim=60, num_heads=[6, 6, 6, 6], mlp_ratio=2, upsampler=args.upsampler, resi_conv='1conv',mean=mean,std=std,gating_layers=args.gating_layers,gating_method=args.gating_method).to(device,dtype=data_type),
+            # "PASR_MLP_G_aug":PASR_MLP_G_aug(upscale=args.scale_factor, in_chans=1, img_size=args.crop_size, window_size=8, depths=[6, 6, 6, 6, 6, 6], embed_dim=180, num_heads=[6, 6, 6, 6, 6, 6], mlp_ratio=2, upsampler=args.upsampler, resi_conv='1conv',mean=mean,std=std).to(device,dtype=data_type),
+            # "PASR_MLP_G_aug_small":PASR_MLP_G_aug(upscale=args.scale_factor, in_chans=1, img_size=args.crop_size, window_size=8, depths=[6, 6, 6, 6], embed_dim=60, num_heads=[6, 6, 6, 6], mlp_ratio=2, upsampler=args.upsampler, resi_conv='1conv',mean=mean,std=std).to(device,dtype=data_type),
+
+    }
+    model = torch.nn.DataParallel(model_list[args.model]).to(device)
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+    savedpath = str(str(args.model) +
+                "_data_" + str(args.data) + 
+                "_crop_size_" + str(args.crop_size) +
+                "_ode_step_" + str(args.ode_step) +
+                "_ode_method_" + str(args.ode_method) +
+                "_task_dt_" +  str(args.task_dt) + 
+                "_num_snapshots_" + str(args.n_snapshot) +
+                "_upscale_factor_" + str(args.scale_factor) +
+                "_timescale_factor_" + str(args.timescale_factor) +
+                "_loss_type_" + str(args.loss_type) +
+                "_lamb_" + str(args.lamb) +
+                "_lr_" + str(args.lr) +
+                "_gamma_" + str(args.gamma) +
+                "_normalizaiton_" + str(args.normalization) + str(ID)
+                ) 
+
+    run = neptune.init(
+    project="junyiICSI/PASR",
+    api_token="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiI2NGIxYjI4YS0yNDljLTQwOWMtOWY4YS0wOGNhM2Q5Y2RlYzQifQ==",
+    tags = ID,
+    )  # your credentials
+
+    run["config"] = vars(args)   
+    min_RFNE,best_epoch = train(args,model, trainloader, val1_loader,val2_loader, optimizer, device, savedpath,run)
+    run["metric/min_RFNE"].log(min_RFNE)
+    run['metric/best_epoch'].log(best_epoch)
+    run.stop()
+    logging.info("Training complete.")
